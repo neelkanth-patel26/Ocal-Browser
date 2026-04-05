8b6876402105d406839ecd18c0dc9257f0f79abf
Neelkanth <neelkanthpatel26@outlook.com>
2026-04-06 03:26:55 +0530

    chore: Release V3.4.40E beta - YouTube Dislike & Elite AdBlock

--- deploy_release.ps1
+++ deploy_release.ps1
@@ -1,7 +1,7 @@
 $token = $env:GH_TOKEN
 $owner = "neelkanth-patel26"
 $repo = "Ocal-Browser"
-$tag = "v3.3.10-beta"
+$tag = "v3.4.40-beta"
 
 $releaseData = Get-Content "release_info.json" -Raw | ConvertFrom-Json
 
@@ -30,7 +30,7 @@ try {
     
     # Artifacts to upload
     $artifacts = @(
-        "dist-inno/Ocal-3.3.10-beta-Setup.exe"
+        "dist-inno/Ocal-3.4.40E-beta-Setup.exe"
     )
 
     foreach ($file in $artifacts) {
