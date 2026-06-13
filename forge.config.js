ec265e8c340d45b0b9f1a579e0e2e4fc7145adcf
Neelkanth <neelkanthpatel26@outlook.com>
2026-06-14 01:41:12 +0530

    chore: bump version to 6.8.00, enable automated packaging, and update installer icon configuration

--- forge.config.js
+++ forge.config.js
@@ -4,7 +4,7 @@ const { FuseV1Options, FuseVersion } = require('@electron/fuses');
 module.exports = {
   packagerConfig: {
     asar: true,
-    icon: './icon.png',
+    icon: './icon.ico',
     ignore: [
       /^\/out($|\/)/,
       /^\/dist-inno($|\/)/,
